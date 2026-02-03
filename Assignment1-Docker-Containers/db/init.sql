1beaa0a5b02fad1d54d1f979b0900ff920756f3b
Himanshi Shrivas <hshriva1@charlotte.edu>
2026-02-03 18:12:32 -0500

    Add Assignment1-Docker-Containers with Docker setup for app and db services

---
+++ Assignment1-Docker-Containers/db/init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE trips (
+  id SERIAL PRIMARY KEY,
+  city TEXT NOT NULL,
+  minutes INT NOT NULL,
+  fare NUMERIC(6,2) NOT NULL
+);
+
+INSERT INTO trips (city, minutes, fare) VALUES
+('Charlotte', 12, 12.50),
+('Charlotte', 21, 20.00),
+('New York', 9, 10.90);
